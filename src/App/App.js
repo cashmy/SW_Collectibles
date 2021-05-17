b23946e96032b2f172a57763247c9a4535a1341c
cashmy <cmyers880@gmail.com>
2021-05-17 11:14:44 -0500

    Moved App to directory under src

---
+++ src/App/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import jwtDecode  from "jwt-decode";
+import AppBar from '../components/AppBar/AppBar.js';
+import Footer from '../components/Footer/Footer.js';
+import Profile from '../components/Profile/Profile.js';
+import SignInSide from '../components/SignInSide/SignInSide.js';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
+class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    const jwt = localStorage.getItem('token')
+    try{
+      const user = jwtDecode(jwt);
+      this.setState({user: user})
+    } catch {}
+  }
+
+  render() {
+    const user = this.state.user;
+    return (
+      <div >
+          <CssBaseline />
+          <AppBar user={user} />
+          <Switch>
+            <Route path="/profile" render = { props => {
+                if (!user){
+                  return <Redirect to="/login" />
+                } else {
+                  return <Profile {...props} user={user} />
+                }
+              }}
+            />
+            <SignInSide />
+          </Switch>
+          <Footer />
+      </div>
+    );
+  }
+}
+export default App;
