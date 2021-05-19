2b36eaa820cd61505047d7635e6663df28c3a928
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-19 10:52:28 -0800

    Fixed error in service layer. Updated axios call in SignInSide.

--- src/Services/serviceLayer.js
+++ src/Services/serviceLayer.js
@@ -25,7 +25,8 @@ class ServiceLayer {
     }
 
     editUser(id, data){
-        return axios.put(`https://localhost:44394/api/user/${id}/edit`, data);
+        const jwt = localStorage.getItem('token')
+        return axios.put(`https://localhost:44394/api/user/${id}/edit`,{headers: {Authorization: 'Bearer ' + jwt}}, data);
     }
 
     setUserRole(id){
@@ -156,7 +157,7 @@ class ServiceLayer {
     }
 
     deleteOrderHeader(id){
-        return axios.delete(`https://localhost:44394/api/OrderHeader/${id}`
+        return axios.delete(`https://localhost:44394/api/OrderHeader/${id}`)
     }
 
 }
