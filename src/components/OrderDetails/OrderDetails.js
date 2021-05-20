13a62abd490e0e5a7959ff3f4ce04e7afcc31f9a
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-20 11:38:01 -0800

    Updated serviceLayer. Removed map function from return in ProductDetailView.

--- src/components/OrderDetails/OrderDetails.js
+++ src/components/OrderDetails/OrderDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid';
--- src/components/ProductsDetailPage/ProductsDetailView.jsx
+++ src/components/ProductsDetailPage/ProductsDetailView.jsx
@@ -5,38 +5,84 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import ServiceLayer from '../../Services/serviceLayer';
 import { Button } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 
 
 function ProductsDetailView(props) {
-    const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState([]);
     const [reviews, setReviews] = useState([]);
 
+    let url = props.location.pathname;
+    let urlSplit = url.split("/");
+    const productId = parseInt(urlSplit[2]);
+
     useEffect(() => {
-        getProducts();
-        getReviews();
+        getProduct(productId);
+        getReviews(productId);
     }, [])
 
-    async function getProducts(e){
+    async function getProduct(id){
         try{
-            const response = await ServiceLayer.getAllProducts();
-            setProducts(response.data);
+            const response = await ServiceLayer.getProductById(id);
+            setProduct(response.data);
         }
         catch(e){
-            console.log('API call unsuccessful', e)
+            console.log('API call unsuccessful', e.response.data)
         }
     }
 
-    async function getReviews(e){
+    async function getReviews(id){
         try{
-            const response = await ServiceLayer.getAllReviews();
+            const response = await ServiceLayer.getReviewByProductId(id);
             setReviews(response.data);
         }catch(e){
             console.log('API call unsuccessful', e)
         }
     }
 
+    const mapProductName = (i) => {
+        if(product){
+            return (
+                <div style={{textAlign: 'center'}}>
+                    <h2 className="product__title">{product.productName}</h2>
+                    <p >{product.productDescription}</p>
+                </div>
+            )
+        }
+    }
+
+    const mapProductDetails = () => {
+        if (product){
+            return (
+                <div className="product__details-right" >
+                    <ul style={{listStyle:"none"}}>
+                        <li>Price: ${product.productPrice}</li>
+                        <li>Rating: {product.productAverageRating} out of 5</li>
+                        <li>Inventory: {product.quantityOnHand} remaining!</li>
+                        <li><Button style={{backgroundColor: '#9C27B0'}} className="addToCart__btn" variant="outlined" >Add To Cart</Button></li>
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+    const mapReviews = () => {
+        return (
+            reviews.map((review, i) => (
+                <div key={i} className="product__reviews">
+                    <Typography noWrap>
+                    <span><span>UserName: </span>{review.reviewText}&nbsp;&nbsp;<span>  UserRating: {review.reviewRating} out of 5</span></span>
+                    </Typography>
+                </div>
+            ))
+
+        )
+
+    }
+
     return (
         <div>
+        <Paper>
         <Grid container direction="row" spacing={5} style={{marginTop: "2rem"}}></Grid>
             <Grid container direction="row" justify="center" alignItems="center" spacing={5}>
                 <Grid container item xs={4} spacing={3} className="grid" direction='column' alignItems="center" justify="center">
@@ -46,34 +92,10 @@ function ProductsDetailView(props) {
                 
                 </Grid>
                 <Grid container item xs={4} spacing={3} justify="center" alignItems="center" className="grid" id="midProductDetailGrid">
-                    {products ? products.map((product,i) => {
-                        while(i == 0){
-                            return (
-                                <div key={i} style={{textAlign: 'center'}}>
-                                    <h2 className="product__title">Han Solo Action Figure </h2>
-                                    <p>{product.productDescription}</p>
-                                </div>
-                            )
-                        }
-                    }): <></>}
+                    {mapProductName()}
                 </Grid>
                 <Grid container item xs={4} spacing={3} zeroMinWidth justify="center" alignItems="center" className="grid">
-                {products ? products.map((product,i) => {
-                        while(i == 0){
-                            return (
-                                <div key={i} className="product__details-right">
-                                        <ul style={{listStyle:"none"}}>
-                                            <li>Price: ${product.productPrice}</li>
-                                            <li>Rating: {product.productAverageRating} out of 5</li>
-                                            <li>Inventory: {product.quantityOnHand} remaining!</li>
-                                            <li><Button style={{backgroundColor: '#9C27B0'}} className="addToCart__btn" variant="outlined" >Add To Cart</Button></li>
-                                        </ul>
-                                </div>
-                            )
-                        }
-                    }): <></>}
-
-                    
+                    {mapProductDetails()}
                 </Grid>
             </Grid>
         <Grid container direction="row" spacing={1} style={{margin: "2rem"}}>
@@ -94,20 +116,12 @@ function ProductsDetailView(props) {
                     autoFocus
                     />
                 </form>
-                {reviews ? reviews.map((review, i) => {
-                                while(i == 0){
-                                    return (
-                                        <div key={i} className="product__reviews">
-                                            <Typography noWrap>
-                                            <span><span>UserName: </span>{review.reviewText}&nbsp;&nbsp;<span>  UserRating: {review.reviewRating} out of 5</span></span>
-                                            </Typography>
-                                        </div>
-                                    )
-                                }
-                            }): <></>}
+                {mapReviews()}
             </div>
         </Grid>
+        </Paper>
         </div>
+        
     )
 }
 
