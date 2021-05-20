0abe1e953efbaacb915eedf170948f4bf52c9766
cashmy <cmyers880@gmail.com>
2021-05-20 14:29:34 -0500

    Add routing to Address Form

--- src/App/App.js
+++ src/App/App.js
@@ -12,6 +12,7 @@ import OrderList from '../components/OrderList/OrderList'
 import OrderDetails from '../components/OrderDetails/OrderDetails'
 import addProducts from "../components/AddProducts/addProducts"
 import RegistrationSide from "../components/RegistrationSide/RegistrationSide"
+import AddressForm from "../components/CheckOut/AddressForm"
 import shoppingCart from "../components/ShoppingCart/shoppingCart"
 import Paper from '@material-ui/core/Paper';
 import BackgroundImage from '../assets/images/wallpapersden.com_star-wars-skywalker-saga_3840x2400.jpg';
@@ -53,9 +54,9 @@ class App extends Component {
                 }
               }}
             />
-        
+            <Route path="/checkOut" component={AddressForm} />
             <Route exact={true} path="/registration" component={RegistrationSide}/> 
-            <Route exact={true} path="/productDetails" component={ProductsDetailView} />
+            <Route path="/productDetails" component={ProductsDetailView} />
             <Route exact={true} path="/productList" component={ProductList} />
             <Route exact={true} path="/orderDetails" component={OrderDetails}/>
             <Route exact={true} path="/orderList" component={OrderList} />
