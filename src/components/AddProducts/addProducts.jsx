3d34c42f5b20a0089f30c591a6178d65e6e252c9
cashmy <cmyers880@gmail.com>
2021-05-22 19:19:27 -0500

    Styled add Product

--- src/components/AddProducts/addProducts.jsx
+++ src/components/AddProducts/addProducts.jsx
@@ -5,17 +5,20 @@ import Grid from '@material-ui/core/Grid';
 import Controls from '../controls/Controls';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import Paper from '@material-ui/core/Paper';
+import PageHeader from '../PageHeader/PageHeader';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import ListIcon from '@material-ui/icons/List';
 // import { LocalConvenienceStoreOutlined, SettingsInputSvideoRounded } from '@material-ui/icons';
 // import serviceLayer from '../../Services/serviceLayer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
+    flexGrow: 1,
+    // '& .MuiTextField-root': {
+    //   margin: theme.spacing(1),
+    //   width: '25ch',
+    // },
   },
   layout: {
     width: 'auto',
@@ -25,6 +28,22 @@ const useStyles = makeStyles((theme) => ({
       width: 800,
       marginLeft: 'auto',
       marginRight: 'auto',
+    },
+  },
+  grid: {
+    padding: theme.spacing(2.5),
+    marginTop: theme.spacing(2.5),
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
     },
   },
 }));
@@ -48,12 +67,12 @@ export default function FormPropsTextFields() {
     event.preventDefault();
     debugger;
     const data = {
-    productDescription: product.productDescription,
-    productPrice:product.productPrice,
-    quantityOnHand: product.quantityOnHand,
-    categoryId: product.categoryId,
-    productAverageRating: product.productAverageRating,
-    productName: product.productName
+      productDescription: product.productDescription,
+      productPrice:product.productPrice,
+      quantityOnHand: product.quantityOnHand,
+      categoryId: product.categoryId,
+      productAverageRating: product.productAverageRating,
+      productName: product.productName
   }
   
   try{
@@ -94,11 +113,11 @@ export default function FormPropsTextFields() {
         ...product, categoryId: parseInt(e.target.value)
       })
     }
-    const onChangeProductAverageRating = (e) => {
-      setProduct({
-        ...product, productAverageRating: parseFloat(e.target.value)
-      })
-    }
+    // const onChangeProductAverageRating = (e) => {
+    //   setProduct({
+    //     ...product, productAverageRating: parseFloat(e.target.value)
+    //   })
+    // }
     const onChangeProductName = (e) => {
       setProduct({
         ...product, productName: e.target.value
@@ -106,45 +125,70 @@ export default function FormPropsTextFields() {
 
     }
   return (
-    <form className={classes.layout} noValidate onSubmit={handleSubmit}>
-      <div>
-        <Paper>
-        <Grid> <TextField required id="standard-required"
-      label="Product Name" 
-      defaultValue="" 
-      value={product.productName} 
-      onChange={onChangeProductName}/>
+    <div className={classes.layout}>
+      <PageHeader 
+        title="Add Product"
+        subtitle="Seller - Add a new product to your inventory."
+        icon={<ListIcon/>}
+      />  
+      <form noValidate onSubmit={handleSubmit}>
+        <Paper classsName={classes.paper}>
+        <Grid className={classes.grid} container spacing={2}>
+        <Grid item xs={12}> 
+          <TextField
+            variant="outlined"
+            required id="standard-required"
+            label="Product Name" 
+            fullWidth
+            defaultValue="" 
+            value={product.productName} 
+            onChange={onChangeProductName}/>
       </Grid>
-       <Grid> <TextField required id="standard-required"
-      label="Product Description" 
-      defaultValue="" 
-      value={product.productDescription} 
-      onChange={onChangeProductDescription}/>
+       <Grid item xs={12}> 
+         <TextField 
+          variant="outlined"
+          required id="standard-required"
+          label="Product Description" 
+          fullWidth
+          defaultValue="" 
+          value={product.productDescription} 
+          onChange={onChangeProductDescription}/>
       </Grid>
-       <Grid> <TextField required id="standard-required"
-       label="Product Price"
-        defaultValue=""
-        value={product.productPrice}
-        onChange={onChangeProductPrice} />
+      <Grid item xs={12}> 
+        <TextField 
+          variant="outlined"
+          required id="standard-required" 
+          label="Category"
+          defaultValue="" 
+          value={product.categoryId}
+          onChange={onChangeCategoryId}/>
+       </Grid>
+       <Grid item xs={6} > 
+         <TextField 
+            variant="outlined"
+            required id="standard-required"
+            label="Product Price"
+            defaultValue=""
+            value={product.productPrice}
+            onChange={onChangeProductPrice} />
         </Grid>
-       <Grid> <TextField required id="standard-required" 
-       label="Quantity"
-       helperText="* means that entry is required" 
-       defaultValue="" 
-       value={product.quantityOnHand}
-       onChange={onChangeQuantityOnHand}/></Grid>
-      <Grid> <TextField required id="standard-required" 
-       label="Category Id"
-       helperText="* means that entry is required" 
-       defaultValue="" 
-       value={product.categoryId}
-       onChange={onChangeCategoryId}/></Grid>
-        <Grid> <TextField required id="standard-required" 
+       <Grid item xs={6}> 
+         <TextField 
+            variant="outlined"
+            required id="standard-required" 
+            label="Quantity"
+            helperText="* means that entry is required" 
+            defaultValue="" 
+            value={product.quantityOnHand}
+            onChange={onChangeQuantityOnHand}/>
+       </Grid>
+
+        {/* <Grid> <TextField required id="standard-required" 
        label="Average Rating"
        helperText="* means that entry is required" 
        defaultValue="" 
        value={product.productAverageRating}
-       onChange={onChangeProductAverageRating}/></Grid>
+       onChange={onChangeProductAverageRating}/></Grid> */}
        <Grid>  <Controls.Button 
                 type="submit"
                 text="Add"
@@ -160,9 +204,10 @@ export default function FormPropsTextFields() {
             /></Grid>
        {/* TODO: add upload image field */}
        {/* <Grid> <ImgField required id="standard-required" label="upload image of product" defaultValue="" /></Grid> */}
+       </Grid>
        </Paper>
-      </div>
     </form>
+    </div>
   );
 }
 
